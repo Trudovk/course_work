e55f059160a1544381f13eb0ad5c252ee695f50d
Trudovk <Trydovk@yandex.ru>
2023-11-27 01:40:32 +0300

    пагинация

--- drf/views.py
+++ drf/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from course_work.models import Category, Item
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from .serializers import ItemSerializer, CategorySerializer
+from collections import OrderedDict
+
+class AdvicePagination(PageNumberPagination):
+    page_size = 5
+    page_sizer_query_param = 'paginate_by'
+    max_page_size = 20
 
 class ItemViewSet(viewsets.ModelViewSet):
     """
@@ -11,6 +19,7 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = AdvicePagination
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """
